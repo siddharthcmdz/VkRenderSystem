f30609d19574de01fafa0c798d59d432edfd332b
siddharthcmdz <siddharth.cmdz@gmail.com>
2023-04-05 19:03:01 -0700

    added support for physical and logic device initiation

--- main.cpp
+++ main.cpp
@@ -5,9 +5,18 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
+#include <optional>
 
 class HelloTriangleApplication {
 private:
+	struct QueueFamilyIndices {
+		std::optional<uint32_t> graphicsFamily;
+
+		bool isComplete() {
+			return graphicsFamily.has_value();
+		}
+	};
+
 	const uint32_t WIDTH = 800;
 	const uint32_t HEIGHT = 600;
 	const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
@@ -15,6 +24,9 @@ private:
 	GLFWwindow* iwindow;
 	VkInstance iinstance;
 	VkDebugUtilsMessengerEXT idebugMessenger;
+	VkPhysicalDevice iphysicalDevice = VK_NULL_HANDLE;
+	VkDevice idevice;
+	VkQueue igraphicsQueue;
 
 #ifdef NDEBUG
 	const bool ienableValidationLayers = false;
@@ -58,11 +70,11 @@ private:
 
 	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
 		createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-		createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |
+		createInfo.messageSeverity = /*VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT |*/
 			VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT |
 			VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
 
-		createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |
+		createInfo.messageType = /*VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT |*/
 			VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT |
 			VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
 		createInfo.pfnUserCallback = debugCallback;
@@ -119,6 +131,60 @@ private:
 		return extensions;
 	}
 
+	bool isDeviceSuitable(VkPhysicalDevice device) {
+		VkPhysicalDeviceFeatures features;
+		VkPhysicalDeviceProperties props;
+		vkGetPhysicalDeviceFeatures(device, &features);
+		vkGetPhysicalDeviceProperties(device, &props);
+		std::cout <<"\n" << props.deviceName << std::endl;
+		std::cout << "================" << std::endl;
+		std::cout << "Driver version: " << props.driverVersion << std::endl;
+		std::cout << "Device Type: " << props.deviceType << std::endl;
+		std::cout << "\nFeatures" << std::endl;
+		std::cout << "===========" << std::endl;
+		std::cout << "multiDrawIndirect: " << features.multiDrawIndirect<< std::endl;
+		std::cout << "shaderInt64: " << features.shaderInt64 << std::endl;
+		std::cout << "fragmentStoresAndAtomics: " << features.fragmentStoresAndAtomics << std::endl;
+		
+		QueueFamilyIndices indices = findQueueFamilies(device);
+		return indices.isComplete();
+	}
+
+	void pickPhysicalDevice() {
+		uint32_t deviceCount = 0;
+		vkEnumeratePhysicalDevices(iinstance, &deviceCount, nullptr);
+		std::vector<VkPhysicalDevice> devices(deviceCount);
+		vkEnumeratePhysicalDevices(iinstance, &deviceCount, devices.data());
+		for (const auto& device : devices) {
+			if (isDeviceSuitable(device)) {
+				iphysicalDevice = device;
+				break;
+			}
+		}
+
+		if (iphysicalDevice == VK_NULL_HANDLE) {
+			throw std::runtime_error("failed to find a suitable card");
+		}
+	}
+
+	QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) {
+		QueueFamilyIndices indices;
+		uint32_t queueFamilyCount = 0;
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+		std::vector<VkQueueFamilyProperties> queueFamilyProperties(queueFamilyCount);
+		vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilyProperties.data());
+
+		for (uint32_t i = 0; i < static_cast<uint32_t>(queueFamilyProperties.size()); ++i) {
+			const VkQueueFamilyProperties& queueFamilyProp = queueFamilyProperties[i];
+			if (queueFamilyProp.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+				indices.graphicsFamily = i;
+				break;
+			}
+		}
+
+		return indices;
+	}
+
 	void createInstance() {
 		if (ienableValidationLayers && !checkValidationLayerSupport()) {
 			std::runtime_error("Validation layers requested by not available!");
@@ -171,9 +237,48 @@ private:
 		
 	}
 
+	void createLogicalDevice() {
+		QueueFamilyIndices indices = findQueueFamilies(iphysicalDevice);
+		
+		VkDeviceQueueCreateInfo queueCreateInfo{};
+		queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+		queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+		queueCreateInfo.queueCount = 1;
+
+		float queuePriority = 1.f;
+		queueCreateInfo.pQueuePriorities = &queuePriority;
+
+		VkDeviceCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+		createInfo.pQueueCreateInfos = &queueCreateInfo;
+		createInfo.queueCreateInfoCount = 1;
+
+		VkPhysicalDeviceFeatures deviceFeatures{};
+		createInfo.pEnabledFeatures = &deviceFeatures;
+		createInfo.enabledExtensionCount = 0;
+
+		if (ienableValidationLayers) {
+			createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+			createInfo.ppEnabledLayerNames = validationLayers.data();
+		}
+		else {
+			createInfo.enabledLayerCount = 0;
+		}
+
+		const VkResult result = vkCreateDevice(iphysicalDevice, &createInfo, nullptr, &idevice);
+		if (result != VK_SUCCESS) {
+			throw std::runtime_error("failed to create logical device");
+		}
+
+		//get the graphics queue handle from the logical device.
+		vkGetDeviceQueue(idevice, indices.graphicsFamily.value(), 0, &igraphicsQueue);
+	}
+
 	void initVulkan() {
 		createInstance();
 		setupDebugMessenger();
+		pickPhysicalDevice();
+		createLogicalDevice();
 	}
 
 	void mainLoop() {
@@ -183,6 +288,8 @@ private:
 	}
 
 	void cleanup() {
+
+		vkDestroyDevice(idevice, nullptr);
 		if (ienableValidationLayers) {
 			DestroyDebugUtilsMessengerEXT(iinstance, idebugMessenger, nullptr);
 		}
