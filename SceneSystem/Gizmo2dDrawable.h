2000ee8f5d4e0d85d0e5571d04a2bacd9c83c4c5
siddharthcmdz <siddharth.cmdz@gmail.com>
2024-04-15 21:28:47 -0700

    updates to remove license stub

--- SceneSystem/Gizmo2dDrawable.h
+++ SceneSystem/Gizmo2dDrawable.h
@@ -1,14 +1,3 @@
-//// =================================================================================
-// Copyright (c) 2023, See All Surgical Inc. All rights reserved.
-//
-// This software is the property of See All Surgical Inc. The software may not be reproduced,
-// modified, distributed, or transferred without the express written permission of See All Surgical Inc.
-//
-// In no event shall See All Surgical Inc. be liable for any claim, damages or other liability,
-// whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software
-// or the use or other dealings in the software.
-// =================================================================================
-
 #pragma once
 
 #include "AbstractWorldDrawable.h"
